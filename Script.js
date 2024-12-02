985e66027b82a94f5b8324f5f8269d4c5e97c54d
Alicev-itoria <Avss13@aluno.ifal.edu.br>
2024-12-02 11:04:19 -0300

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,9 @@
+let valorProduto = parseFloat(prompt('Digite um valor de um produto: '));
+
+let reajuste = valorProduto * 0.3333;
+
+let somaValores = valorProduto + reajuste;
+
+alert('O valor do seu reajuste é de: ' + reajuste)
+
+alert('O seu saldo com o reajuste é de: ' + somaValores);
